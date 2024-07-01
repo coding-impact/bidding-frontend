c77a3e29b804da9478e78facb92b5476ddeff2c3
Kulimi <kulimi@kulimi.tw>
2024-07-01 12:54:52 +0800

    fix: fix vite-plugin-vue-layouts is missing in pacakge

---
+++ src/pages/admin.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-container class="d-flex text-center justify-center align-center h-100 flex-column">
+        <v-card title="競標系統" subtitle="籌碼至上主義教室" width="500" class="mb-4">
+          
+        </v-card>
+        <router-link to="/" class="text-caption text-grey">點此前往競標頁面</router-link>
+      </v-container>
+</template>
+<script>
+</script>
